1e84a5df33cda2726800c8a0aa945a44122a4f16
razieh i <izanloo@amwajco.net>
2025-10-08 12:59:13 +0330

    refactor: remove font

--- frontend/my-app/src/app/layout.jsx
+++ frontend/my-app/src/app/layout.jsx
@@ -1,17 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import Providers from "./providers";
+import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -22,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
           <Toaster />
